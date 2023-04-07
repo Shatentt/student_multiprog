fcd296d3a8deda2d08aa556ba8f848acb85616fb
Emilia <emilykreydlina@yandex.ru>
2023-04-07 19:13:17 +0300

    темы по физике и математике

--- flask_app.py
+++ flask_app.py
@@ -11,12 +11,6 @@ app = Flask(__name__)
 
 HEADERS = {"User-Agent": UserAgent().random}
 
-url = "https://educon.by/index.php/formuly/formmat"
-response = requests.get(url, headers=HEADERS)
-sp = BeautifulSoup(response.text, 'lxml')
-data = sp.find('div', itemprop="articleBody")
-themes = data.find('ul').text.split('\n')[1:-5]
-
 
 @app.route("/")
 def index():
@@ -25,7 +19,30 @@ def index():
 
 @app.route('/formulas')
 def formulas():
-    return render_template('formulas.html', title='Формулы', list=themes)
+    # math
+    url_math = "https://educon.by/index.php/formuly/formmat"
+    response_math = requests.get(url_math, headers=HEADERS)
+    sp_math = BeautifulSoup(response_math.text, 'lxml')
+    data_math = sp_math.find('div', itemprop="articleBody")
+    themes_math = data_math.find('ul').text.split('\n')[1:-5]
+    a = data_math.find_all('img')
+    imgs_formulas_math = []
+    for elem in a:
+        imgs_formulas_math.append('https://educon.by' + elem.get('src'))
+    ###
+
+    # physics
+    url_physics = "https://educon.by/index.php/formuly/formfiz"
+    response_physics = requests.get(url_physics, headers=HEADERS)
+    sp_physics = BeautifulSoup(response_physics.text, 'lxml')
+    data_physics = sp_physics.find('div', itemprop="articleBody")
+    themes_physics = data_physics.find('ul').text.split('\n')[1:-2]
+    a = data_physics.find_all('img')
+    imgs_formulas_physics = []
+    for elem in a:
+        imgs_formulas_physics.append('https://educon.by' + elem.get('src'))
+    ###
+    return render_template('formulas.html', title='Темы', list_math=themes_math, list_physics=themes_physics)
 
 
 def main():
