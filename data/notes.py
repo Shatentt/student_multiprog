052e7281cb41dfad92c339e5b2b3f47d3dfa0ef3
Arslan Farkhitov <arsfarkhitov@gmail.com>
2023-04-08 22:22:32 +0300

    notes added

---
+++ data/notes.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Note(db.Model):
+    __tablename__ = 'notes'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Note('{self.title}', '{self.content}', '{self.created_at}')"
--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,52 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+from forms.NoteForm import NoteForm
+from data.notes import db, Note
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notes.db'
+db.init_app(app)
+
 
 @app.route("/")
 def index():
     return render_template("main_page.html")
+
+
+@app.route('/notes', methods=['GET', 'POST'])
+def notes():
+    form = NoteForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        content = form.content.data
+        note = Note(title=title, content=content)
+        db.session.add(note)
+        db.session.commit()
+        return redirect(url_for('notes'))
+    notes = Note.query.all()
+    return render_template('notes.html', form=form, notes=notes)
+
+
+@app.route('/notes/<int:id>', methods=['GET', 'POST'])
+def edit_note(id):
+    note = Note.query.get_or_404(id)
+    form = NoteForm(obj=note)
+    if form.validate_on_submit():
+        form.populate_obj(note)
+        db.session.commit()
+        return redirect(url_for('notes'))
+    return render_template('edit_note.html', form=form, note=note)
+
+
+@app.route('/notes/delete/<int:id>', methods=['POST'])
+def delete_note(id):
+    note = Note.query.get_or_404(id)
+    db.session.delete(note)
+    db.session.commit()
+    return redirect(url_for('notes'))
+
 
 
 def main():
