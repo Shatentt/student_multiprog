f39aa4b7c6ec1468525c1f3ffa697ec70ac05bc0
Emilia <emilykreydlina@yandex.ru>
2023-04-11 04:44:06 +0300

    формулы по математике

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 
 import requests
 from bs4 import BeautifulSoup
@@ -10,6 +10,8 @@ from fake_useragent import UserAgent
 app = Flask(__name__)
 
 HEADERS = {"User-Agent": UserAgent().random}
+imgs_formulas_physics = []
+imgs_formulas_math = {}
 
 
 @app.route("/")
@@ -24,11 +26,22 @@ def formulas():
     response_math = requests.get(url_math, headers=HEADERS)
     sp_math = BeautifulSoup(response_math.text, 'lxml')
     data_math = sp_math.find('div', itemprop="articleBody")
-    themes_math = data_math.find('ul').text.split('\n')[1:-5]
+    themes_math = data_math.find('ul').text.split('\n')[1:-4]
     a = data_math.find_all('img')
-    imgs_formulas_math = []
+    b = []
     for elem in a:
-        imgs_formulas_math.append('https://educon.by' + elem.get('src'))
+        b.append('https://educon.by' + elem.get('src'))
+    imgs_formulas_math['Формулы сокращенного умножения'] = b[:9]
+    imgs_formulas_math['Квадратное уравнение и формула разложения квадратного трехчлена на множители'] = b[9:17]
+    imgs_formulas_math['Свойства степеней и корней'] = b[20:36]
+    imgs_formulas_math['Формулы с логарифмами'] = b[36:49]
+    imgs_formulas_math['Арифметическая прогрессия'] = b[49:54]
+    imgs_formulas_math['Геометрическая прогрессия'] = b[54:60]
+    imgs_formulas_math['Тригонометрия'] = b[60:100]
+    imgs_formulas_math['Тригонометрические уравнения'] = b[100:113]
+    imgs_formulas_math['Геометрия на плоскости (планиметрия)'] = b[113:169]
+    imgs_formulas_math['Геометрия в пространстве (стереометрия)'] = b[169:183]
+    imgs_formulas_math['Координаты'] = b[183:187]
     ###
 
     # physics
@@ -38,11 +51,15 @@ def formulas():
     data_physics = sp_physics.find('div', itemprop="articleBody")
     themes_physics = data_physics.find('ul').text.split('\n')[1:-2]
     a = data_physics.find_all('img')
-    imgs_formulas_physics = []
     for elem in a:
         imgs_formulas_physics.append('https://educon.by' + elem.get('src'))
     ###
     return render_template('formulas.html', title='Темы', list_math=themes_math, list_physics=themes_physics)
+
+
+@app.route('/formulas/<theme>')
+def themes(theme):
+    return render_template('theme.html', title=theme, theme=theme, list_math=imgs_formulas_math)
 
 
 def main():
