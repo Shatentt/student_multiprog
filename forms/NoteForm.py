a04f5dfb99f2b045dc1b85472f98e03eac23a314
Arslan Farkhitov <arsfarkhitov@gmail.com>
2023-04-13 11:22:32 +0300

    design changed

--- forms/NoteForm.py
+++ forms/NoteForm.py
@@ -3,6 +3,6 @@ from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class NoteForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired(), Length(max=100)])
-    content = TextAreaField('Content', validators=[Length(max=1000)])
-    submit = SubmitField('Create Note')
+    title = StringField('Название', validators=[DataRequired(), Length(max=100)])
+    content = TextAreaField('Содержание', validators=[Length(max=1000)])
+    submit = SubmitField('Создать заметку')
